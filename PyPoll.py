b2f1cfb646ce4d75062e8b07a96834b7a9a48a0e
Duvey <rudow.david@gmail.com>
2020-06-16 13:58:02 -0500

    added candidate names

--- PyPoll.py
+++ PyPoll.py
@@ -19,12 +19,36 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 #where to save the output to
 file_to_save = os.path.join("analysis","election_analysis.txt")
 
+#set a variable to hold vote counts
+total_votes = 0
+
+#make a list for the cadidates names
+candidate_options = [ ]
+
+
+#open the elections results and read the file
 with open(file_to_load) as election_data:
    file_reader = csv.reader(election_data)
+   
+   #print the header row
    headers = next(file_reader)
    print(headers)
-   # for row in next(file_reader):
-   #    print(row)
+
+   #for each vote cast
+   for row in file_reader:
+      #add the vote to the total number of votes
+      total_votes += 1
+      
+      #who are they voting for?
+      candidate_name = row[2]
+      
+      #create a list of candidates
+      if candidate_name not in candidate_options:
+         candidate_options.append(row[2])
+
+   
+print(total_votes)
+print(candidate_options)
 
 # with open(file_to_save,"w") as election_data:
 #    election_data.write("Hello World")
