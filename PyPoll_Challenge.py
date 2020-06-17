1be0092e9a8de33fe85cda9ab107a39f0dd1da42
Duvey <rudow.david@gmail.com>
2020-06-17 13:37:03 -0500

    added county results and print to terminal

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -27,6 +27,24 @@ winning_total = 0
 winning_percentage = 0
 winning_candidate = ""
 
+#make a list fo the county names
+county_names = []
+
+#make a dictionary to store the votes cast in each county
+county_votes = {}
+
+#store the county they are voting in
+county_name = ""
+
+county_vote = 0
+county_percentage = 0
+
+county_results = ""
+
+#variable to hold largest county
+largest_county = ""
+largest_county_vote = 0
+
 #make a list for the cadidates names
 candidate_options = [ ]
 
@@ -47,17 +65,29 @@ with open(file_to_load) as election_data:
       
       #who are they voting for?
       candidate_name = row[2]
-      
+
       #create a list of candidates
       if candidate_name not in candidate_options:
          #add the candidate to the candidate list
-         candidate_options.append(row[2])
+         candidate_options.append(candidate_name)
          #start tracking their totals
-         candidate_votes[row[2]] = 0
+         candidate_votes[candidate_name] = 0
       
       #add the vote to the candidates vote total
-      candidate_votes[row[2]] += 1
-      
+      candidate_votes[candidate_name] += 1
+
+      #county they are voting in
+      county_name = row[1]
+
+      #check if the county is in the list
+      if county_name not in county_names:
+         county_names.append(county_name)
+         county_votes[county_name] = 0
+
+      #add the vote to the county vote total
+      county_votes[county_name] += 1
+
+
 #write the results to the file
 with open(file_to_save,"w") as election_data:
    #header for the file
@@ -65,9 +95,39 @@ with open(file_to_save,"w") as election_data:
    f"Election Results\n"
    f"-------------------------\n"
    f"Total Votes: {total_votes:,}\n"
-   f"-------------------------\n")
-   election_data.write(election_results, end = "")
+   f"-------------------------\n"
+   f"County Results:\n")
+   election_data.write(election_results)
+   print(election_results)
 
+   #tabulate county results
+   for county in county_names:
+      #get county vote count
+      county_vote = county_votes[county]
+      
+      #calcultate vote percentage
+      county_percentage = round(county_vote/total_votes*100,1)
+      
+      #find the county with the largest vote
+      if county_vote > largest_county_vote:
+         largest_county_vote = county_vote
+         largest_county = county
+      
+      #save results to file
+      county_results = (f"{county}: {county_percentage}% ({county_vote:,})\n")
+      election_data.write(county_results)
+      print(county_results)
+
+   #save county results
+   county_results =(
+    f"-------------------------\n"
+    f"Largest County Turnout: {largest_county} ({largest_county_vote:,})\n"
+    f"-------------------------\n")
+   election_data.write(county_results)
+   print(county_results)
+
+   election_data.write("Candidate Results:\n")
+   print("Candidate Results:\n")
    #tabulate the totals and find the winner
    for candidate in candidate_votes:
       #number of votes the candidate received
@@ -84,7 +144,8 @@ with open(file_to_save,"w") as election_data:
       
       #print each candidates results
       candidate_results = (f"{candidate}: {vote_percentage}% ({votes:,})\n")
-      election_data.write(candidate_results) 
+      election_data.write(candidate_results)
+      print(candidate_results) 
    
    #print winning candidate
    winning_candidate_summary = (
@@ -94,3 +155,4 @@ with open(file_to_save,"w") as election_data:
     f"Winning Percentage: {winning_percentage:.1f}%\n"
     f"-------------------------\n")
    election_data.write(winning_candidate_summary)
+   print(winning_candidate_summary)
